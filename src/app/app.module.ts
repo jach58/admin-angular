ac61f13ca4ff943bd1ee9555e748bfb7f8dd6be8
Square <jach58@hotmail.com>
2018-03-26 18:08:22 -0600

    Input, Output, ViewChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
@@ -12,18 +13,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     PagesModule,
-    APP_ROUTES
+    APP_ROUTES,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 
 //Modulos
 import { SharedModule } from '../shared/shared.module'
+
+//ng2-charts
+import { ChartsModule } from 'ng2-charts'
 
 import { PagesComponent } from './pages.component'
 import { DashboardComponent } from "./dashboard/dashboard.component"
@@ -9,13 +13,18 @@ import { ProgressComponent } from "./progress/progress.component"
 import { Graficas1Component } from "./graficas1/graficas1.component"
 import { PAGES_ROUTES } from "./pages.routes"
 
+//temporal
+import { IncrementadorComponent } from './../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -24,7 +33,9 @@ import { PAGES_ROUTES } from "./pages.routes"
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
