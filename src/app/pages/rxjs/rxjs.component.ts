52438fca7175ffbfc7b8def4717a5ca28a9ab56c
Square <jach58@hotmail.com>
2018-03-27 00:52:08 -0600

    Seccion 8 terminada

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription : Subscription
+
+  constructor() {
+
+    this.subscription = this.regresaObservable()
+      .subscribe(
+        num => console.log('Subs', num), 
+        error => console.error('Error en el obs', error),
+        () => console.log('El observador terminó')
+      )
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
+  }
+
+  regresaObservable(): Observable<any> {
+    return new Observable( observer => {
+      let contador = 0
+      let intervalo = setInterval(() =>{
+        contador += 1
+        
+        let salida = {
+          valor: contador
+        }
+
+        observer.next(salida)
+
+        // if(contador === 3){
+        //   clearInterval(intervalo)
+        //   observer.complete()
+        // }
+
+        // if(contador === 2){
+        //   observer.error('Error!!')
+        // }
+      }, 500)
+    })
+    .retry(2)
+    .map((res:any) => res.valor)
+    .filter((num,index) => {
+      if((num %2) == 1){
+        return true
+      }else{
+        return false
+      }
+    })
+  }
+
+}
