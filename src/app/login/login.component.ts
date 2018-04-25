543fa0760fb03e8343aa663ce2128b6ef3061254
Square <jach58@hotmail.com>
2018-04-25 03:15:16 -0500

    Seccion 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins()
+declare const gapi: any
 
 @Component({
   selector: 'app-login',
@@ -9,15 +13,61 @@ declare function init_plugins()
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string
+  recuerdame: boolean = false
 
-  constructor(public router: Router) { }
+  auth2: any
+
+  constructor(public _usuarioService: UsuarioService ,public router: Router) { }
 
   ngOnInit() {
     init_plugins()
+    this.googleInit()
+
+    this.email = localStorage.getItem('email') || ''
+    if(this.email.length > 1){
+      this.recuerdame = true
+    }
+
   }
 
-  ingresar() {
-    this.router.navigate(['/dashboard'])
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '110100810037-k14dsess2fcuge613ekon2qt547l8buh.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      })
+
+      this.attachSignin(document.getElementById('btnGoogle'))
+
+
+    })
+  }
+
+  attachSignin( element) {
+    this.auth2.attachClickHandler(element, {}, googleUser => {
+
+      //let profile = googleUser.getBasicProfile()
+      let token = googleUser.getAuthResponse().id_token
+      this._usuarioService.loginGoogle(token)
+        .subscribe(()=> window.location.href='#/dashboard')
+    })
+  }
+
+  ingresar(forma: NgForm) {
+
+    if(forma.invalid) {
+      return
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password)
+
+    this._usuarioService.login(usuario, forma.value.recuerdame)
+      .subscribe(correcto => this.router.navigate(['/dashboard']))
+
+    
+    //this.router.navigate(['/dashboard'])
   }
 
 }
